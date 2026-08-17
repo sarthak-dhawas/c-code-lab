fd58deb0fb7d03692840c954c5169b0f525713ef
Sarthak Dhawas <sarthak.dhawas18@gmail.com>
2026-08-17 21:31:13 +0530

    Add files via upload

---
+++ rev a num.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main () {
+    int original_num, reverse_num;
+    printf("Enter your number: ");
+    scanf("%d", &original_num);
+reverse_num = 0;
+
+while(original_num){
+    reverse_num = reverse_num * 10 + original_num % 10;
+    original_num/=10;
+}
+printf("Reversed number: %d", reverse_num);
+return 0;
+}
